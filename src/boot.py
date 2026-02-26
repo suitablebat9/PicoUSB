7b9538d4460b9e0ff22501cf449d85d371cd897b
suitablebat9 <carsondaniel16@gmail.com>
2026-02-26 09:07:38 -0600

    Add files via upload

--- src/boot.py
+++ src/boot.py
@@ -1,18 +1,3 @@
-#   Copyright 2024 PicoUSB
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-"""Pico USB boot.py file. If button is not pressed, execute code.py, not showing as mass storage; if pressed reset into SAFE MODE where code.py is not executed"""
 import time
 import board
 import storage
@@ -22,6 +7,10 @@ import microcontroller
 mode = digitalio.DigitalInOut(board.GP25)
 mode.direction = digitalio.Direction.INPUT
 mode.pull = digitalio.Pull.UP
+
+led = digitalio.DigitalInOut(board.GP12)
+led.direction = digitalio.Direction.OUTPUT
+led.value = True
 
 storage.remount("/", readonly=False)
 m = storage.getmount("/")
